f5a1aba02cab71b1dcc41b09034981231d3d7746
hamdialtinbas <h.altinbass@gmail.com>
2017-08-21 22:58:17 +0300

    Test amaclı

---
+++ src/main/java/merge/helloworld2.java
@@ -0,0 +1,10 @@
+package merge;
+
+public class helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("sadasdads");
+    }
+    public boolean getdata() {
+        System.out.println("sadasdasd");
+    }
+}
